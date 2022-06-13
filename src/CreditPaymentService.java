9e6d9a5720b3b4d73134113f766170b879582a8a
Tigradcom <tigradcom2@gmail.com@>
2022-06-13 11:50:07 +0400

    Reformat code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,10 +1,8 @@
 public class CreditPaymentService {
-    public double calculate (int amount,double percent,double time) {
-        double percentmonth = percent/ (100*12); // месячная процентная ставка
-        double timemonth = time*12; // количество месяцец по кредиту
-        double totalpercent = Math.pow(1+percentmonth,-timemonth);
-        return amount*(percentmonth/(1-totalpercent));
-
-
+    public double calculate(int amount, double percent, double time) {
+        double percentMonth = percent / (100 * 12);                  // месячная процентная ставка
+        double timeMonth = time * 12;                                // количество месяцец по кредиту
+        double totalPercent = Math.pow(1 + percentMonth, -timeMonth);
+        return amount * (percentMonth / (1 - totalPercent));
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+
         CreditPaymentService service = new CreditPaymentService();
-            int amount = 1_000_000;
-            double percent = 9.99;
-            double time = 3;
-            double payment = service.calculate(amount,percent,time);
-            System.out.println("Ежемесячный платеж:");
-            System.out.printf("%8.2f", + payment);
+        double payment1 = service.calculate(1_000_000, 9.99, 1);
+        int finalPayment1 = (int) payment1;
+        System.out.println("Ежемесячный платеж (кредит на 1 год): " + finalPayment1);
+
+        double payment2 = service.calculate(1_000_000, 9.99, 2);
+        int finalPayment2 = (int) payment2;
+        System.out.println("Ежемесячный платеж (кредит на 2 года): " + finalPayment2);
+
+        double payment3 = service.calculate(1_000_000, 9.99, 3);
+        int finalPayment3 = (int) payment3;
+        System.out.println("Ежемесячный платеж (кредит на 3 года): " + finalPayment3);
 
     }
-
 }
 
